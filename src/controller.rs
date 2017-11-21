c1c55a7922400e0e0947efc076d815a2c5cbf0a1
madeindjs <contact@rousseau-alexandre.fr>
2017-11-21 16:37:51 +0100

    Reorganize controller

--- src/controller.rs
+++ src/controller.rs
@@ -2,17 +2,48 @@
 
 pub mod recipes {
     use rocket_contrib::Template;
+    use diesel::prelude::*;
+    use schema::recipes::dsl::*;
+    use diesel::LimitDsl;
+    use diesel::LoadDsl;
+
+    use models;
+    use database;
 
     #[get("/")]
     pub fn index() -> Template {
         let map = ();
+
+        let connection = database::establish_connection();
+        let results = recipes
+            .filter(id.eq(1))
+            .limit(5)
+            .load::<models::Recipe>(&connection)
+            .expect("Error loading recipes");
+
+        println!("Displaying {} recipes", results.len());
+        for recipe in results {
+            println!("{}", recipe.name);
+        }
+
+
         Template::render("index", &map)
     }
 
-    #[get("/<name>")]
-    pub fn show(name: String) -> Template {
-        // let recipe = models::Recipe{name: name};
+    #[get("/<slug>")]
+    pub fn show(slug: String) -> Template {
+        // let recipe = models::Recipe{slug: slug};
         let recipe = ();
         Template::render("recipe", recipe)
+    }
+}
+
+pub mod pages {
+    use rocket_contrib::Template;
+
+    #[get("/")]
+    pub fn home() -> Template {
+        let map = ();
+        Template::render("index", &map)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,40 +14,16 @@ extern crate diesel_codegen;
 
 use rocket_contrib::Template;
 
-use diesel::LimitDsl;
-use diesel::LoadDsl;
-
 
 mod controller;
 mod models;
 mod database;
 mod schema;
 
-use self::diesel::prelude::*;
-use self::schema::recipes::dsl::*;
-
-#[get("/")]
-fn index() -> Template {
-    let connection = database::establish_connection();
-    let results = recipes
-        .filter(id.eq(1))
-        .limit(5)
-        .load::<models::Recipe>(&connection)
-        .expect("Error loading recipes");
-
-    println!("Displaying {} recipes", results.len());
-    for recipe in results {
-        println!("{}", recipe.name);
-    }
-
-    let map = ();
-    Template::render("index", &map)
-}
-
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![controller::pages::home])
         .mount("/recipes", routes![controller::recipes::index])
         .mount("/recipes", routes![controller::recipes::show])
         .attach(Template::fairing())
