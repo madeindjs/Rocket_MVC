3458e5371e927d2fc9acb447979539d3c3f782a2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-07 16:09:42 +0100

    Use JSON objects

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket_contrib::json::Json;
+
+
+
+#[derive(Serialize)]
+struct Recipe {
+    name : String
 }
 
+
+#[get("/")]
+fn index() -> Json<Vec<Recipe>> {
+    let recipes : Vec<Recipe> =  vec![Recipe{name: "Recipe A".to_string()}, Recipe{name: "Recipe B".to_string()}];
+    Json(recipes)
+}
+
+#[get("/a")]
+fn show() -> Json<Recipe> {
+    Json(Recipe{name: "Recipe A".to_string()})
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+    .mount("/", routes![index])
+    .mount("/recipes", routes![show])
+    .launch();
 }
